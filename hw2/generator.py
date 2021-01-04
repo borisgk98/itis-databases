766070a52713be3c1799e4217481b9dc04f19eb3
borisgk98 <borisgk98@ya.ru>
2021-01-05 01:56:05 +0300

    add hw5 images

--- hw2/generator.py
+++ hw2/generator.py
@@ -6,13 +6,19 @@ import sys
 auth_provider = PlainTextAuthProvider(username='admin', password='cassPass1234')
 cluster = Cluster(["192.168.2.166"], port=9042,auth_provider=auth_provider)
 session = cluster.connect()
-session.set_keyspace('test')
+session.set_keyspace('test3')
 
-insert = session.prepare("insert into test.TEST(id, name) values (?, ?)")
+insert = session.prepare("insert into test3.TEST3(id, name) values (?, ?)")
 
-n = 10**4
-k = 10**6
+n = 10**7
+k = 2*10**6
+total_ins = 0
 for id in range(1 + k, k + n + 1):
-    session.execute(insert, [id, get_full_name()])
+    try:
+        session.execute(insert, [id, get_full_name()])
+        total_ins+=1
+        print(total_ins, " ok")
+    except Exception as e:
+        print(e)
 
 sys.exit(0)
